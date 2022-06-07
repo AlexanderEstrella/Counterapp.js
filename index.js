345189606166a95944683178e455e32ed2d9e0d3
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2022-06-07 18:06:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let count = 0;
+let btton = document.getElementById('count-el');
+let savee = document.getElementById('save-el');
+let sevish = document.getElementById('save');
+
+function increment() {
+count += 1;
+btton.innerText = count;
+
+}
+document.getElementById('increase').addEventListener('click', () => {
+    increment();
+})
+
+function saveIt(){
+console.log(count);
+}
+
+document.getElementById('save').addEventListener("click", () => {
+
+    saveIt();
+    savee.innerHTML += " " + count + " - ";
+    btton.innerText = "0"
+})
+
+
