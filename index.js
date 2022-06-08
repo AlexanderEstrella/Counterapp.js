73615e07933b34ee54ebeec1d02c7aee2e0b7718
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2022-06-08 14:03:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,18 @@
 let count = 0;
 let btton = document.getElementById('count-el');
 let savee = document.getElementById('save-el');
-let sevish = document.getElementById('save');
 
 function increment() {
  countTr += 1;
-btton.innerText = count;
+btton.textContent = count
 
 }
-document.getElementById('increase').addEventListener('click', () => {
-    increment();
-})
-
-function saveIt(){
-console.log(count);
+function save(){
+ let countTr = count + '-'
+ savee.textContent += countTr
+ btton.textContent = 0
+ count = 0
+ 
 }
-
-document.getElementById('save').addEventListener("click", () => {
-
-    saveIt();
-    let countTr = count + "-"
-    savee.textContent += countTr;
-     btton.textContent = 0;
-      count = 0;
-})
 
 
