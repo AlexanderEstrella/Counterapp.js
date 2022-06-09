e33fe56e41d9a7bec1fb017064e45cd63537e351
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2022-06-09 08:52:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ let btton = document.getElementById('count-el');
 let savee = document.getElementById('save-el');
 
 function increment() {
- countTr += 1;
-btton.textContent = count
-
+ count += 1;
+btton.textContent = count;
 }
+document.document.getElementById('count-el').addEventListener('click', function () {
+increment();
+
+})
 function save(){
  let countTr = count + '-'
- savee.innerText += countTr
- btton.innerText = 0
- count = 0
- 
+ savee.innerText += countTr;
+ btton.innerText = 0;
+ count = 0;
 }
+document.document.getElementById('count-el').addEventListener('click', function () {
+save();
+
+})
+
 
 
